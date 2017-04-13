7fa8a4e3b2e0c5dd9c926c134659619c1f454f2b
christian99salerno <christian.salerno05@gmail.com>
2017-04-13 18:51:32 +0200

    Aggiornato .gitignore e aggiunto programma stampa asterischi a intervalli di 700 ms

---
+++ asterischi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+
+int main ()
+{
+
+struct timespec intervallo;
+
+intervallo.tv_sec = 0;
+intervallo.tv_nsec = 700 * 1000 * 1000;
+
+puts("*");
+nanosleep(&intervallo, NULL);
+
+puts("*");
+nanosleep(&intervallo, NULL);
+
+puts("*");
+nanosleep(&intervallo, NULL);
+
+return 0;
+}
